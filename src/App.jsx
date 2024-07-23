c24303c9625feb6937e7db194268e7d0a4db3a64
Paulo Gabriel Lima de Sousa <paulogabriellima@hotmail.com>
2024-07-22 22:34:36 -0300

    Botões finalizados, cores padrão finalizadas, início da checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Button from "./components/buttons/Buttons.jsx";
+import Checkbox from "./components/checkbox/Checkbox.jsx";
 
 function App() {
     return (
-        <Button
-            label="BATATA"
-            buttonType="primary-button"
-            
-        />
+        <>
+            <Button label="BATATA" buttonType="primary-button" />
+            <Checkbox />
+        </>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "primereact/resources/themes/bootstrap4-dark-blue/theme.css"
@@ -8,7 +8,5 @@ import "./components/texts/texts.css"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
     <App />
-
 )
